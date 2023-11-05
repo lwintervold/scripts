af6901c5d765e9a9c9274183d45b4427f9a1605e
Lee Wintervold <38053017+lwintervold@users.noreply.github.com>
2023-11-05 13:45:53 -0600

    workg

--- switch_monitors.bat
+++ switch_monitors.bat
@@ -2,13 +2,19 @@ set ComputerName=%1
 
 if "%ComputerName%" == "desktop" (
 	set monitor_value=15
+	goto set_values
 )
 if "%ComputerName%" == "laptop" (
 	set monitor_value=17
-) else (
-	set monitor_value=15
+	goto set_values
 )
 
+:desktop_defaults
+set monitor_1="M1LMQS120278"
+set monitor_2="M1LMQS120281"
+set monitor_value=15
 
-start ControlMyMonitor.exe /SetValue "\\.\DISPLAY2\Monitor0" 60 %monitor_value%
-start ControlMyMonitor.exe /SetValue "\\.\DISPLAY1\Monitor0" 60 %monitor_value%
+
+:set_values
+start ControlMyMonitor.exe /SetValue "M1LMQS120278" 60 %monitor_value%
+start ControlMyMonitor.exe /SetValue "M1LMQS120281" 60 %monitor_value%
